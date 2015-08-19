cae2362b9035f9da3e3d092705a4e80d1ef48347
Jonathan Ventura <jventura@uccs.edu>
2015-08-19 16:05:24 -0600

    Changes to packages

--- ros_install_script.sh
+++ ros_install_script.sh
@@ -2,7 +2,7 @@
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list’;
 wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add -;
 sudo apt-get update;
-sudo apt-get install g++ ros-indigo-ros-base ros-indigo-ros-control ros-indigo-ros-controllers ros-indigo-navigation ros-indigo-teleop-twist-keyboard ros-indigo-vision-opencv ros-indigo-ar-track-alvar ros-indigo-camera-calibration ros-indigo-image-view ros-indigo-image-view2  ros-indigo-rosbag ros-indigo-serial ros-indigo-compressed-image-transport ros-indigo-camera-info-manager;
+sudo apt-get install g++ ros-indigo-ros-base ros-indigo-ros-control ros-indigo-ros-controllers ros-indigo-navigation ros-indigo-teleop-twist-keyboard ros-indigo-vision-opencv ros-indigo-ar-track-alvar ros-indigo-camera-calibration ros-indigo-rosbag ros-indigo-serial;
 sudo apt-get install python-rosdep;
 sudo rosdep init;
 rosdep update;
