2078784827f51ae1a326a86f66df0c17e9560ce3
Jonathan Ventura <jventura@uccs.edu>
2015-08-20 10:56:07 -0600

    Fixed quotes in raspi setup

--- raspi_setup_script.sh
+++ raspi_setup_script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-if [ “$#” -ne 1 ]; then
-  echo “usage: $0 number” > &2;
+if [ "$#" -ne 1 ]; then
+  echo "usage: $0 number" > &2;
   exit 1;
 fi
 sudo apt-get install linux-firmware;
@@ -8,11 +8,11 @@ sudo apt-get install dphys-swapfile;
 sudo apt-get install libraspberrypi-bin libraspberrypi-dev;
 sudo ln -s /usr /opt/vc
 sudo apt-get install wireless-tools;
-sudo echo ‘allow-hotplug wlan0’ >> /etc/network/interfaces;
-sudo echo ‘auto wlan0’ >> /etc/network/interfaces;
-sudo echo ‘iface wlan0 inet static’ >> /etc/network/interfaces;
-sudo echo ‘  address 192.168.0.1’ >> /etc/network/interfaces;
-sudo echo ‘  netmask 255.255.255.0’ >> /etc/network/interfaces;
-sudo echo ‘  wireless-channel $1’ >> /etc/network/interfaces;
-sudo echo ‘  wireless-essid RaspberryPi$1’ >> /etc/network/interfaces;
-sudo echo ‘  wireless-mode ad-hoc’ >> /etc/network/interfaces;
+sudo echo 'allow-hotplug wlan0' >> /etc/network/interfaces;
+sudo echo 'auto wlan0' >> /etc/network/interfaces;
+sudo echo 'iface wlan0 inet static' >> /etc/network/interfaces;
+sudo echo '  address 192.168.0.1' >> /etc/network/interfaces;
+sudo echo '  netmask 255.255.255.0' >> /etc/network/interfaces;
+sudo echo '  wireless-channel $1' >> /etc/network/interfaces;
+sudo echo '  wireless-essid RaspberryPi$1' >> /etc/network/interfaces;
+sudo echo '  wireless-mode ad-hoc' >> /etc/network/interfaces;
