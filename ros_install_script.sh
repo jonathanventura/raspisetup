262084716a9b89e9ab474a23fddd6616f32ae4ee
Jonathan Ventura <jventura@uccs.edu>
2015-08-20 13:34:21 -0600

    Added force overwrite for opengl stuff

--- ros_install_script.sh
+++ ros_install_script.sh
@@ -2,6 +2,11 @@
 sudo sh -c "echo 'deb http://packages.ros.org/ros/ubuntu trusty main' > /etc/apt/sources.list.d/ros-latest.list";
 wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add -;
 sudo apt-get update;
+sudo apt-get -y download libegl1-mesa-dev
+sudo dpkg -i --force-overwrite libegl1-mesa-dev_10.1.3-0ubuntu0.4_armhf.deb
+sudo apt-get -y download libgles2-mesa-dev
+sudo dpkg -i --force-overwrite libgles2-mesa-dev_10.1.3-0ubuntu0.4_armhf.deb
+sudo apt-get install -f
 sudo apt-get -y install g++ ros-indigo-ros-base ros-indigo-ros-control ros-indigo-ros-controllers ros-indigo-navigation ros-indigo-teleop-twist-keyboard ros-indigo-vision-opencv ros-indigo-ar-track-alvar ros-indigo-camera-calibration ros-indigo-rosbag ros-indigo-serial;
 sudo apt-get -y install python-rosdep;
 sudo rosdep init;
@@ -20,6 +25,6 @@ echo "source /home/ubuntu/ros_catkin_ws/devel/setup.bash" >> ~/.bashrc;
 source ~/.bashrc;
 cd src;
 git clone https://github.com/jonathanventura/raspicam_node.git;
-cd ..
+cd ..;
 catkin_make;
 source ~/.bashrc;
